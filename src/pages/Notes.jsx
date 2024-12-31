7a6a52853c48a9b1ec7f5c46d7f2409f6895e385
ilsaa009 <elsatahir5@gmail.com>
2024-12-31 12:10:23 +0500

    first commit

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import NoteForm from "../components/NoteForm";
 import Note from "../components/Note";
 import ThemeOptions from "../components/ThemeOptions";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function Notes() {
     const [notes, setNotes] = useState([]);
@@ -46,7 +47,15 @@ function Notes() {
         return () => clearInterval(interval);
     }, []);
     
-
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+    
+        const items = Array.from(notes);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+    
+        setNotes(items);
+      }
     return (
         <>
             <div>
@@ -59,16 +68,41 @@ function Notes() {
             </div>
             <NoteForm onAddNote={addNote} />
             <p className="date-time">{dateTime}</p>
-            <div>
-                {notes.map((note) => (
-                    <Note
-                        key={note.$id}
-                        noteData={note}
-                        onDelete={() => deleteNote(note.$id)} 
-                        onComplete={() => completeNote(note.$id)}
-                    />
-                ))}
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="notes">
+                    {(provided) =>(
+                        <div 
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="notes-List"
+                        >
+                            {notes.map((note, index) =>( 
+                                <Draggable
+                                key={note.$id}
+                                draggableId={note.$id.toString()}
+                                index={index}
+                                >
+                                    {(provided) => (
+                                        <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        >
+                                            <Note
+                                            noteData={note}
+                                            onDelete={() => deleteNote(note.$id)}
+                                            onComplete={() => completeNote(note.$id)}
+                                            />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+
+            </Droppable>
+    </DragDropContext>
         </>
     );
 }
